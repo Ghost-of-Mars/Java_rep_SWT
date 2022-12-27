90f94e77b3808f9766a2a479b845e3f46e35eab0
Ghost-of-Mars <120248151+Ghost-of-Mars@users.noreply.github.com>
2022-12-28 02:10:20 +0700

    5

--- adressbook-web-tests/src/test/java/addressbook/tests/appmanager/ContactHelper.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/appmanager/ContactHelper.java
@@ -4,32 +4,20 @@ import addressbook.tests.models.ContactData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class ContactHelper {
-    private WebDriver wd;
-
+public class ContactHelper extends HelperBase{
     public ContactHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void submitContactCreation() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        click(By.xpath("//div[@id='content']/form/input[21]"));
     }
 
     public void fillNewContactForm(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.firstName());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.lastName());
-      wd.findElement(By.name("address")).click();
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(contactData.address());
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(contactData.mobilePhone());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.email());
+        type(By.name("firstname"), contactData.firstName());
+        type(By.name("lastname"), contactData.lastName());
+        type(By.name("address"), contactData.address());
+        type(By.name("mobile"), contactData.mobilePhone());
+        type(By.name("email"), contactData.email());
     }
 }
--- adressbook-web-tests/src/test/java/addressbook/tests/appmanager/NavigationHelper.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/appmanager/NavigationHelper.java
@@ -3,22 +3,21 @@ package addressbook.tests.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-    private WebDriver wd;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void goToGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 
     public void goToTheHomeLink() {
-      wd.findElement(By.linkText("home")).click();
+        click(By.linkText("home"));
     }
 
     public void goToTheAddNewContact() {
-      wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
     }
 }
