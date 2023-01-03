1768ced68312348ae16a7b9bae29f04adedcf3ac
Ghost-of-Mars <120248151+Ghost-of-Mars@users.noreply.github.com>
2023-01-03 15:34:56 +0700

    + multiple browsers

--- adressbook-web-tests/src/test/java/addressbook/tests/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/appmanager/ApplicationManager.java
@@ -3,8 +3,10 @@ package addressbook.tests.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
@@ -12,10 +14,19 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        }
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         login("admin", "secret");
         groupHelper = new GroupHelper(wd);
